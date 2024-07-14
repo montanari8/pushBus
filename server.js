502b94c6ae3c7c975553d252bb59e5cfedefc56b
André Luiz Montanari <103907596+montanari8@users.noreply.github.com>
2024-07-14 20:22:59 -0300

    atualização de dependências

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const webpush = require("web-push");
 const bodyParser = require("body-parser");
 const path = require("path");
 const PushNotifications = require("node-pushnotifications");
